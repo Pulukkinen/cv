f219c32fb6a59edd8efa3004ebe60b4f5d4d8ad6
Joonas Pulkkinen <joonastommimatiaspulkkinen@gmail.com>
2017-01-18 21:04:10 +0000

    ITskillit saatu samaan pisteeseen kuin ennen filun korruptiota

--- html/templates/it-skills.php
+++ html/templates/it-skills.php
@@ -1,3 +1,44 @@
-<div class="container-fluid text-center top-buffer">
+<div class="container-fluid text-center top-buffer bottom-buffer">
 	<h3>Programming</h3>
+	<div class="row top-buffer">
+		<div class="col-xs-4">
+			<img src="images/cpp.jpg" width=65% height=65%>
+		</div>
+		<div class="col-xs-4">
+			<img src="images/python.png" width=30% height=30%>
+		</div>
+		<div class="col-xs-4">
+			<img src="images/webprog.jpg" width=35% height=35%>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-xs-4">
+			<p>Basic knowledge, including dynamic memory allocation and some experiece about GUIs</p>
+		</div>
+		<div class="col-xs-4">
+			<p>Python, Basic knowledge about object oriented programming</p>
+		</div>
+		<div class="col-xs-4">
+			<p>Well, this page is the first web page I've ever written, thats pretty much it.
+				The page should also be  mobile friendly, at least for the newest devices</p>
+		</div>
+	</div>
+<div class="container-fluid text-center top-buffer" id="operating-content">
+	<h3>Operatin systems</h3>
+
+	<div class="row top-buffer">
+		<div class="col-xs-6">
+			<img src="images/winlogo.jpg" width=25% height=25%>
+		</div>
+		<div class="col-xs-6">
+			<img src="images/linuxlogo.png" width=22% height=22%>
+		</div>
+	<div class="row">
+		<div class="col-xs-6">
+			<p>Windows, I use it allmost every day, so I am very familiar with it, includin MS Office</p>
+		</div>
+		<div class="col-xs-6">
+			<p>Linux, I'm still learning, but I'm handling basic terminal usage quite well. This web page is hosted from
+			Linux based server, which I have set up</p>
 </div>
